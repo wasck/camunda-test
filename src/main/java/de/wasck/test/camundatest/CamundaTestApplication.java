82215f1dc3c88c25ad50a02cb7c7d15b05a426f4
wasck <Waldemar.Schik@gmail.com>
2020-07-04 23:30:48 +0200

    Init projact
    Initialized spring boot project

---
+++ src/main/java/de/wasck/test/camundatest/CamundaTestApplication.java
@@ -0,0 +1,13 @@
+package de.wasck.test.camundatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/wasck/test/camundatest/CamundaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package de.wasck.test.camundatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
